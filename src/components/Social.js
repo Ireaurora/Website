db355a36c2fa176f8d665d81ecb0f810cd735961
Ireaurora <ireaurora@gmail.com>
2020-04-08 17:51:45 +0100

    Added links for social media

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,8 +4,8 @@ class Social extends Component {
     render() {
         return (
             <div class="social">
-            <a href="https://github.com/" target="_blank"><i class="fab fa-github"></i></a>
-            <a href="https://Linkedin.com/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://github.com/Ireaurora" target="_blank"><i class="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/irene-s-234966125/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
             <a href="https://twitter.com/WinnieIre" target="_blank"><i class="fab fa-twitter"></i></a>
             </div>
             )
