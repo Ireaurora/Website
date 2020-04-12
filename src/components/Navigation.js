4c289c83032931c227aaf09e073aa382d04079b3
Ireaurora <ireaurora@gmail.com>
2020-04-12 18:46:48 +0100

    Additional changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,8 @@ class Navigation extends Component {
 
     render() {
         return (
-        <Navbar collapseOnSelect expand="lg">
+
+        <Navbar collapseOnSelect expand="lg" className="navbar navbar-fixed-top scrolling-navbar double-nav mobile-nofixed">
             <Navbar.Brand href="/">Irene Sarigu</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
@@ -15,8 +16,10 @@ class Navigation extends Component {
                  <Nav.Link href="/About">About</Nav.Link>
                  <Nav.Link href="/Education">Education</Nav.Link>
                  <Nav.Link href="/Skills">Skills</Nav.Link>
-                 <Nav.Link href="/Contact">Contact</Nav.Link>
             </Nav>
+            <Nav>
+                <Nav.Link href="/Contact">Contact</Nav.Link>
+              </Nav>
             </Navbar.Collapse>
             </Navbar>
         
