446bdf7e8913c96013be0ed8e822e51c83a06300
Ireaurora <ireaurora@gmail.com>
2020-04-16 17:06:07 +0100

    Skills part working

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -1,33 +1,30 @@
 import React, { Component } from 'react'
 
 class FlipCard extends Component {
-  constructor(props)
-    {
-        super(props);
-        this.state={
-            'new': this.props.myskills
-        };
-       //this.state.new.map(x => console.log(x));
-       //console.log(this.state.new);
-      // {console.log(this.state.new)}
-    }
-    render( 
-    ) {
+    render() {
+      const List= (props) => {
+        while(true){
+        if(props.tasks != undefined){
+        return <p>{props.tasks.join(', ')}</p>}
+        else{
+          return <p>Not present</p>
+        }
+      }
+      };
         return (
             <div className="flip-card">
             <div className="flip-card-inner">
               <div className="flip-card-front">
               <p className="title">{this.props.title}</p>
               <p> @ </p>
-              <p className="where">{this.props.where}</p> 
+              <p classßName="where">{this.props.where}</p> 
               <img src={this.props.image} className="flip-card-image"/>
                <p className="date">{this.props.from} - {this.props.to}</p>
               </div>
               <div className="flip-card-back">
-                
-                <ul>
-          
-                </ul>
+
+              <List tasks= {this.props.skills} />
+
               </div>
             </div>
           </div>
--- src/contents/Work.js
+++ src/contents/Work.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import FlipCard from '../components/FlipCard';
 
-
 class Work extends Component {
     render() {
         return (
             <div className="condiv">
             <h1 className="subtopic" id="work"><span>Work</span></h1>
     
-            <FlipCard title="Web Developer" where="Legend Club Managment System" from="Sept 2019" to="April 2020" image="" myskills="[HTML','CSS','JS','PHP']"/>
+            <FlipCard title="Web Developer" where="Legend Club Managment System" from="Sept 2019" to="April 2020" image="" skills={['HTML', 'CSS', 'JS', 'PHP']}/>
            
             <FlipCard title="Software Engineer Intern" where="Legend Club Managment System" from="July 2019" to="Sept 2019" image=""/>
 
