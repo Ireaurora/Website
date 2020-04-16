f8a43080c6b90655bece204b37118d26d37f3ac6
Ireaurora <ireaurora@gmail.com>
2020-04-16 17:13:00 +0100

    Accidentally deleted the skills section

--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 class Skills extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            'myskills':['HTML','CSS','JS','PHP']
+        };
+    }
+
     render() {
         return (
             <div className="condiv skills">
             <h1 className="subtopic" id="skills"><span>My Skills</span></h1>
+            <ul>
+            {this.state.myskills.map((value)=>{
+                return <li>{value}</li>
+            })}
+            </ul>
             <ul></ul>
             </div>
             )
