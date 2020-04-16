85050f7190bd2285f9135328903fb9a68fb016d2
Ireaurora <ireaurora@gmail.com>
2020-04-16 15:41:23 +0100

    Fixing classname console errors

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react'
 
 class FlipCard extends Component {
-    render() {
+  constructor(props)
+    {
+        super(props);
+        this.state={
+            'new': this.props.myskills
+        };
+       //this.state.new.map(x => console.log(x));
+       //console.log(this.state.new);
+      // {console.log(this.state.new)}
+    }
+    render( 
+    ) {
         return (
-            <div class="flip-card">
-            <div class="flip-card-inner">
-              <div class="flip-card-front">
-              <p>{this.props.title}</p>
+            <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+              <p className="title">{this.props.title}</p>
               <p> @ </p>
-              <p>{this.props.where}</p> 
-              <img src={this.props.image} class="flip-card-image"/>
-               <p>{this.props.from} - {this.props.to}</p>
+              <p className="where">{this.props.where}</p> 
+              <img src={this.props.image} className="flip-card-image"/>
+               <p className="date">{this.props.from} - {this.props.to}</p>
               </div>
-              <div class="flip-card-back">
-                <p>{this.props.education}</p>
+              <div className="flip-card-back">
+                
+                <ul>
+          
+                </ul>
               </div>
             </div>
           </div>
             )
         }
     }
-    
+
 export default FlipCard
     
--- src/contents/Work.js
+++ src/contents/Work.js
@@ -8,7 +8,7 @@ class Work extends Component {
             <div className="condiv">
             <h1 className="subtopic" id="work"><span>Work</span></h1>
     
-            <FlipCard title="Web Developer" where="Legend Club Managment System" from="Sept 2019" to="April 2020" image=""/>
+            <FlipCard title="Web Developer" where="Legend Club Managment System" from="Sept 2019" to="April 2020" image="" myskills="[HTML','CSS','JS','PHP']"/>
            
             <FlipCard title="Software Engineer Intern" where="Legend Club Managment System" from="July 2019" to="Sept 2019" image=""/>
 
