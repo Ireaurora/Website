4f1f4c8cf4eff2df27de2424d9831b273157bf43
Ireaurora <ireaurora@gmail.com>
2020-06-03 14:48:26 +0100

    smooth scroll works on desktop

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import $ from "jquery";
 
 class Navigation extends Component {
+    componentDidMount() {
+        // Markup should load first
+        console.log("hello");
+        $('.navbar-links').click(function() {
+            console.log("hi")
+            var sectionTo = $(this).attr('href');
+            console.log($(this));
+            $('html, body').animate({
+              scrollTop: $(sectionTo).offset().top
+            }, 1500);
 
+
+        });
+      }
     render() {
         return (
 
@@ -12,17 +26,16 @@ class Navigation extends Component {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-                 <Nav.Link href="#about">About</Nav.Link>
-                 <Nav.Link href="#work">Experience</Nav.Link>
+                 <Nav.Link className="navbar-links" href="#about">about</Nav.Link>
+                 <Nav.Link className="navbar-links" href="#work">experience</Nav.Link>
                  {/* <Nav.Link href="#skills">Skills</Nav.Link> */}
             </Nav>
             <Nav>
-                <Nav.Link href="#education">Education</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
+                <Nav.Link className="navbar-links" href="#education">education</Nav.Link>
+                <Nav.Link  className="navbar-links" href="#contact">contact</Nav.Link>
               </Nav>
             </Navbar.Collapse>
             </Navbar>
-        
             )
         }
     }
