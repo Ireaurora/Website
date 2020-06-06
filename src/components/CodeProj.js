c413fbf77aeff8a5c3b12c3d90c7eee55c485b32
Ireaurora <ireaurora@gmail.com>
2020-06-06 17:56:06 +0100

    Styling changes

--- src/components/CodeProj.js
+++ src/components/CodeProj.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 
 class CodeProj extends Component {
     render() {
         return (
             <Card>
-              <Card.Img className="Cardimage" variant="top" src={this.props.image} />
+              <Card.Img className="Cardimage" variant="top" src={this.props.image}/>
               <Card.Body>
                 <Card.Title>{this.props.title}</Card.Title>
                 <Card.Text>{this.props.text}</Card.Text>
-                <a href="https://ireaurora.github.io/Temperature_converter/" target="_blank" className="btn btn-primary card_button">Github Repo</a>
-                <a href="https://ireaurora.github.io/Temperature_converter/" target="_blank" className="btn btn-primary card_button">{this.props.name}s</a>
+                <Button className="card_button" variant="primary">Code</Button>
+                <Button className="card_button" variant="primary">Live</Button>
               </Card.Body>
               <Card.Footer>
-                  <p>Developed in ~> {this.props.when}</p>
+                  <p id="small">Developed in ~> {this.props.when}</p>
               </Card.Footer>
             </Card>
         )
--- src/contents/CodeProjects.js
+++ src/contents/CodeProjects.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import CardDeck from 'react-bootstrap/CardDeck'
 import uniofYork from '../img/uniOfYork.png';
 import CodeProj from '../components/CodeProj';
+import Contact from '../contents/Contact';
 
 class CodeProject extends Component {
     render() {
         return (
-            <div className="CodeProjects">
+            <div className="condiv">
+            <div className="CodeCards">
             <CardDeck>
                 <CodeProj title="P1" when="2018" image={uniofYork} text="Some modules" />
                 <CodeProj title="Computer Science" when="2018" image={uniofYork} text="Some modules" name="jij"/>
@@ -17,6 +19,8 @@ class CodeProject extends Component {
                 <CodeProj title="Computer Science" when="2018" image={uniofYork} text="Some modules" name="jij" />
                 <CodeProj title="Computer Science" when="2018" image={uniofYork} text="Some modules" name="jij" />
             </CardDeck>
+        </div>
+        <Contact />
         </div>
             )
         }
